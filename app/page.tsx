fc3ff9182f1a7f8a01740fe4459de9f7d92df973
Zahid Allaulddin <zahidalaaudin@gmail.com>
2025-02-07 12:42:34 +0300

    update favicon and add loading for index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { redirect } from "next/navigation";
 import { useAuth } from "@/contexts/AuthContext";
 
 export default function Home() {
-	const { isLoggedIn } = useAuth();
+	const { isLoggedIn, isLoading } = useAuth();
+
+	if (isLoading)
+		return (
+			<div className="flex flex-col min-h-screen justify-center">
+				<LoadingSpinner className="h-19 w-19 mx-auto" />
+			</div>
+		);
+
 	return isLoggedIn ? redirect("/dashboard") : redirect("/login");
 }
